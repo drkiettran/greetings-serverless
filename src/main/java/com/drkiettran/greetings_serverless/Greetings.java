bd29a7ed7497aa7462a746e0ef6eefa995362148
drtran <DrKietTran@gmail.com>
2019-04-26 07:13:11 -0400

    Added .gitigore, added greetings code from older project

--- src/main/java/com/drkiettran/greetings_serverless/Greetings.java
+++ src/main/java/com/drkiettran/greetings_serverless/Greetings.java
@@ -1,20 +1,17 @@
 package com.drkiettran.greetings_serverless;
 
-import java.util.Map;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.lang.management.ManagementFactory;
 
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
+/**
+ * Greetings class
+ * 
+ */
+public class Greetings {
+	public String hello(String name) throws UnknownHostException {
+		return String.format("Hello, '%s' from %s/%s", name, InetAddress.getLocalHost().getHostName(),
+				ManagementFactory.getRuntimeMXBean().getName());
 
-public class Greetings implements RequestHandler<Map<String, Object>, Object> {
-
-	public Object handleRequest(Map<String, Object> input, Context context) {
-		context.getLogger().log("input: " + input.get("name") + "\n");
-		StringBuilder sb = new StringBuilder("{");
-		sb.append("\"message\":\"hello ");
-		sb.append(input.get("name"));
-		sb.append("\"}");
-//		String greeting = "{ \"message\": \"Hello " + input.get("name") + "\" }";
-		return sb.toString();
 	}
-
 }
--- src/main/java/com/drkiettran/greetings_serverless/Main.java
+++ src/main/java/com/drkiettran/greetings_serverless/Main.java
@@ -1,5 +1,7 @@
 package com.drkiettran.greetings_serverless;
 
+import java.net.UnknownHostException;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
@@ -8,7 +10,14 @@ public class Main implements RequestHandler<HandlerRequest, HandlerResponse> {
 	@Override
 	public HandlerResponse handleRequest(HandlerRequest input, Context context) {
 		context.getLogger().log("Hello " + input.getName());
-		return new HandlerResponse("Hello " + input.getName());
+		String greetings = "";
+		try {
+			greetings = new Greetings().hello(input.getName());
+		} catch (UnknownHostException e) {
+			context.getLogger().log(e.getMessage());
+		}
+		context.getLogger().log("returning: " + greetings);
+		return new HandlerResponse(greetings);
 	}
 
 }
